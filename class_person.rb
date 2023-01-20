c5fbf578ff362b17dfe9be5e25687dd9478a2e6d
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-01-20 18:07:34 +0000

    Class with first name, age, last name, and full name defined

--- class_person.rb
+++ class_person.rb
@@ -6,21 +6,43 @@
 #  - An instance method, full_name, that puts first_name and last_name together
 #  - An instance method, age, that calculates the number of years between today and birthdate
 
-class Person
+  class Person
+    attr_accessor :first_name
+    attr_accessor :last_name
+    attr_accessor :birthdate
+    
+    def full_name
+      return self.first_name + " " + self.last_name
+      end
+    
+      require "date"
+     def age
+      dob=Date.parse(self.birthdate)
+      today=Date.today
+      days_age=today-dob
+      days_years=days_age/365
+
+      return days_years.to_i
+     end
+  end
   
-end
+  c = Person.new
+  c.first_name = "Jack"
+  c.last_name = "Huguenin"
+  c.birthdate= "November 20,2000"
+  
+p c.age
+ 
 
 # Test your instance methods by adding to the end of this file:
 
-#  new_person = Person.new
-#  new_person.first_name = "Joe"
-#  new_person.last_name = "Mama"
-#  new_person.full_name 
-# Output:
-#=>  "Joe Mama"
+ new_person = Person.new
+ new_person.first_name = "Joe"
+ new_person.last_name = "Mama"
+ p new_person.full_name 
 
-#  other_person = Person.new
-#  other_person.birthdate = "April 19, 1987"
-#  other_person.age
-# Output:
-#=>  45
+
+ other_person = Person.new
+ other_person.birthdate = "April 19, 1987"
+ p other_person.age
+
